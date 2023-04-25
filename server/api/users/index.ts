cf9f7cdafdd7d8b6ce73b721f4f70963e18254d6
saymakoley <97885143+saymakoley@users.noreply.github.com>
2023-04-25 08:26:25 +0100

    implement api route for creating new user

--- server/api/users/index.ts
+++ server/api/users/index.ts
@@ -1,7 +1,34 @@
 import { db } from '../../db'
+import { v4 as uuid } from 'uuid'
+import { sendError } from 'h3'
 
 export default defineEventHandler(async (e) => {
-    if (e.req.method === 'GET') {
+
+    if (e.node.req.method === 'POST') {
+        const body = await useBody(e)
+
+        if (!body.name) {
+            const badRequestErr = createError({
+                statusCode: 400,
+                statusMessage: "Name property not found"
+            })
+
+            sendError(e, badRequestErr)
+        }
+
+        const newUser = {
+            id: uuid(),
+            name: body.name, 
+            email: body.email,
+            password: body.password,
+            online: true,
+        }
+
+        db.users.push(newUser)
+        return newUser
+    }
+
+    if (e.node.req.method === 'GET') {
         return db.users
     }
 })
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,3 +1,11 @@
+interface User {
+    id: string,
+    name: string,
+    email: string, 
+    password: string,
+    online: boolean
+}
+
 export const db = {
-    users: []
+    users: [] as User[],
 }
